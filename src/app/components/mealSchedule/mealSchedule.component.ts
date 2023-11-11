d4e4d3e19fc41a8fec4c92ce4f0efd6c60bf0e74
acsecsi <andras.csecsi@bca.hu>
2023-11-11 01:05:08 +0100

    fix: fix last bits

--- src/app/components/mealSchedule/mealSchedule.component.ts
+++ src/app/components/mealSchedule/mealSchedule.component.ts
@@ -12,25 +12,6 @@ import { Component, Input, OnInit } from '@angular/core';
 // 		-Please note that all dates between start and end dates are considered dates for stay.
 // 		-You can assume that start and end dates will always be in the same month.
 
-// Logic Task
-// Set a blue background for the row that contains the guest name of the Longest Substring Without Repeating Characters.
-
-// 		Example 1:
-// 		Input: s = "Anna"
-// 		Output: 2
-// 		Explanation: The answer is "An", with the length of 2.
-
-// 		Example 2:
-// 		Input: s = "bbbbb"
-// 		Output: 1
-// 		Explanation: The answer is "b", with the length of 1.
-
-// 		Example 3:
-// 		Input: s = "pwwkew"
-// 		Output: 3
-// 		Explanation: The answer is "wke", with the length of 3.
-// 		Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
-
 @Component({
   selector: 'meal-schedule',
   templateUrl: './mealSchedule.component.html',
